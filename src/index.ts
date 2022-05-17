d62d983af2e57348e3803f866446c50080c1e085
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-16 21:34:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+//let months1: string[] = new Array(12);
+//months1[0] = "enero";
+
+function mesesDelAnio() {
+  let meses: string[] = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
+  console.log(meses[0]);
+}
+mesesDelAnio();
