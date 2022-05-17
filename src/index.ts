d42d104d8b12517e9d79d78cc1024b1b77896b6e
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-16 21:39:47 -0300

    aplicamos numeros

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-//let months1: string[] = new Array(12);
-//months1[0] = "enero";
-
-function mesesDelAnio() {
-  let meses: string[] = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre"
-  ];
-  console.log(meses[0]);
+function numerosRandom() {
+  let numeros: number[] = [20, 14, 8, 0, 5, 19, 24];
+  console.log(numeros);
 }
-mesesDelAnio();
+numerosRandom();
